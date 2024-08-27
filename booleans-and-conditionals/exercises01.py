f7cb51d18a7dd2800c2c0e7dc3a44f704bd76534
Sabirah Suhaimi <ssuhaimibira@gmail.com>
2024-08-27 15:01:07 -0500

    with a message

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,6 +1,10 @@
 # Assign the variables for exercise 1 here:
-
-
+engine_indicator_light = "red blinking" 
+space_suits_on = True 
+shuttle_cabin_ready = True 
+crew_status = space_suits_on and shuttle_cabin_ready 
+computer_status_code = 200 
+shuttle_speed = 15000
 
 # BEFORE running the code, predict what will be printed to the console by the following statements:
 
