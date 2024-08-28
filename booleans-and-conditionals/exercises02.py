bbc804dbf379ea3e1c1f965cc0683478ca1379a4
Sabirah Suhaimi <ssuhaimibira@gmail.com>
2024-08-28 16:13:24 -0500

    used else and if statements to write conditionals about launching a shuttle

--- booleans-and-conditionals/exercises02.py
+++ booleans-and-conditionals/exercises02.py
@@ -8,14 +8,29 @@ shuttle_speed = 15000
 # 3) Write conditional expressions to satisfy the following safety rules:
 
 # a) If crew_status is true, print "Crew Ready" else print "Crew Not Ready".
-
+if crew_status:
+    print("Crew Ready")
+else: 
+    print("Crew Not Ready")
 
 # b) If computer_status_code is 200, print "Please stand by. Computer is rebooting." Else if computer_status_code is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
 
+if computer_status_code == 200:
+    print("Please stand by. Computer is rebotting")
+elif computer_status_code == 400:
+    print ("Success! Computer offline!")
+else:
+    print("ALERT:Computer offine!")   
 
 # c) If shuttle_speed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttle_speed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
-
+if shuttle_speed > 17500:
+    print("ALERT: Escape velocity reached!")
+elif shuttle_speed < 8000:
+    print("ALERT: Cannot maintain orbit!")
+else:
+    print("Stable speed")
 
 # 4) PREDICT: Do the code blocks shown in the Section D produce the same result?
 
 # print("Yes" or "No")
+Yes 
--- booleans-and-conditionals/exercises03.py
+++ booleans-and-conditionals/exercises03.py
@@ -19,12 +19,52 @@ engine_temperature = 1200
 
 # Code 5a - 5f here:
 
+if fuel_level > 20000 and engine_temperature <= 2500:
+    print("Full tank. Engines good")
 
+elif fuel_level > 10000 and engine_temperature <= 2500:
+    print("Fuel level above 50%. Engines good")
+
+elif fuel_level > 5000 and engine_temperature <= 2500:
+    print ("Fuel level above 25%. Engines good.")
+
+elif fuel_level <= 5000 or engine_temperature > 2500:
+    print("Check fuel level. Engines running hot.")
+
+elif fuel_level < 1000 or engine_temperature > 3500 or engine_indicator_light == "red blinking":
+    print("ENGINE FAILURE IMMINENT")
+
+else:
+    print("Fuel and engine status pending...")
+
+# A different way to see if it makes a difference
+
+if fuel_level < 1000 or engine_temperature > 3500 or engine_indicator_light == "red blinking":
+    print("ENGINE FAILURE IMMINENT")
+
+elif fuel_level > 10000 and engine_temperature <= 2500:
+    print("Fuel level above 50%. Engines good")
+
+elif fuel_level > 20000 and engine_temperature <= 2500:
+    print("Full tank. Engines good")
+
+elif fuel_level <= 5000 or engine_temperature > 2500:
+    print("Check fuel level. Engines running hot.")
+
+elif fuel_level > 5000 and engine_temperature <= 2500:
+    print ("Fuel level above 25%. Engines good.")
+
+else:
+    print("Fuel and engine status pending...")
 
 # 6) a) Create the variable command_override, and set it to be true or false. If command_override is false, then the shuttle should only launch if the fuel and engine check are OK. If command_override is true, then the shuttle will launch regardless of the fuel and engine status.
+command_override = True
 
 
 # 6) b) Code the following if/else check:
+if (fuel_level > 20000 and engine_indicator_light != "red blinking") or command_override == "True":
+    print("Cleared to launch")
+else: print("Launch scrubbed")
 
 # If fuel_level is above 20000 AND engine_indicator_light is NOT red blinking OR command_override is true print "Cleared to launch!" Else print "Launch scrubbed!"
 
